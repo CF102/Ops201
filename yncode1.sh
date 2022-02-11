cba94cabfee69fee9b1a6e9726307d4492e8c712
Yuriy <youardesign@gmail.com>
2022-02-11 07:03:57 +0000

    practice

---
+++ yncode1.sh
@@ -0,0 +1,12 @@
+# shebang /bin/bash
+
+# Script Name:OpsChallengeCode
+# Description of purpose:automate performance
+# Example usage:repetitious commands
+# Expected output:saving resources
+# Declaration of variables (if any):
+# Declaration of functions (if any):
+
+# Main
+
+# End
