af1c4cb8903d9e7daae229fa5e6af88743661b80
Yuriy <youardesign@gmail.com>
2022-02-11 06:00:45 +0000

    created a file

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+# shebang /bin/bash
+
+# Script Name:OPsChallenge02
+# Description of purpose:
+# Example usage:
+# Expected output:
+# Declaration of variables (if any):
+# Declaration of functions (if any):
+
+# Main
+
+# End
