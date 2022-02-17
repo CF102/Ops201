fdbb62f6cbf9ceeb9790f2a10bcda4ea4e4092a2
Yuriy <youardesign@gmail.com>
2022-02-17 05:32:05 +0000

    Arrays.sh

--- OpsChal04/Arrays.sh
+++ OpsChal04/Arrays.sh
@@ -1,14 +1,14 @@
-# shebang /bin/bash
-
-# Script Name:arrays
-# Description of purpose:learn how to create arrays
-# Example usage:automation of script
-# Expected output:shorten script running time 
-# Source of bsh code: https://linuxize.com/post/bash-concatenate-strings/
+# dirlist=("dir1" "dir2" "dir3" "dir4")
+# # echo ${dirlist[*]}
+# mkdir ${dirlist[@]}
 # Main
-# Create veriables
-var1=$(mkdir dir1)
+pd1=$(home/yuriy/Ops201/OpsChal04/dir1)
+pd2=$(home/yuriy/Ops201/OpsChal04/dir2)
+pd3=$(home/yuriy/Ops201/OpsChal04/dir3)
+pd4=$(home/yuriy/Ops201/OpsChal04/dir4)
 
+pdall=("pd1" "pd2" "pd3" "pd4")
 
+echo "$pdall"
 
 # End
